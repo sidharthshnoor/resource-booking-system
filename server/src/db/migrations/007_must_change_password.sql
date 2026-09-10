fe14db5975b0a0e9e76c85a849697891364ba56d
sidarthshnoor <sidharthn@shnoor.com>
2026-09-10 14:31:28 +0530

    migrations update

--- server/src/db/migrations/007_must_change_password.sql
+++ server/src/db/migrations/007_must_change_password.sql
@@ -1,3 +1,5 @@
 ALTER TABLE users
   ADD COLUMN IF NOT EXISTS must_change_password BOOLEAN NOT NULL DEFAULT false;
 
+-- PostgreSQL fills existing rows with the DEFAULT above; it does not set them
+-- to true. New temporary admins explicitly opt into true in application flows.
